dddd9e730adcbd867a3de64f2ebc319001664eb0
0619fafa <510981051@qq.com>
2020-06-27 19:18:11 +0800

    服务层实现1

--- src/main/java/org/nchu/hospital/repository/DoctorRepository.java
+++ src/main/java/org/nchu/hospital/repository/DoctorRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface DoctorRepository extends CrudRepository<Doctor, Long> {
     @Query("select * from doctor where dep_id = :condition")
-    public Doctor findDocByDep(@Param("condition") long depId);
+    public Collection<Doctor> findDocByDep(@Param("condition") long depId);
+
+    @Query("select * from doctor where ran_id = :condition")
+    public Collection<Doctor> findDocByRan(@Param("condition") long ranId);
 
 }
